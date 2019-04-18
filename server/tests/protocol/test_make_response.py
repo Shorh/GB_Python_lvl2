560f2d540f455ee0736cc2f2de0c44f06d46f0a0
Shorh <Shorhina@gmail.com>
2019-04-18 21:50:32 +0500

    используем фикстуры

--- server/tests/protocol/test_make_response.py
+++ server/tests/protocol/test_make_response.py
@@ -1,32 +1,62 @@
+import pytest
+
 from datetime import datetime
 from protocol import make_response
 
 
-def test_make_response():
-    action_name = 'msg'
-    user = {
+@pytest.fixture
+def action_name():
+    return 'presence'
+
+
+@pytest.fixture
+def user():
+    print('Connected')
+    return {
         'username': 'shorh',
         'status': 'on-line'
     }
-    data = 'Some data'
-    code = 400
 
-    request = {
+
+@pytest.fixture
+def time():
+    return datetime.now().timestamp()
+
+
+@pytest.fixture
+def data():
+    print('Connected')
+    return 'Пользователь: shorh. Статус: on-line'
+
+
+@pytest.fixture
+def code():
+    return 400
+
+
+@pytest.fixture
+def valid_request(action_name, user, time):
+    return {
         'action': action_name,
         'user': user,
-        'time': datetime.now().timestamp(),
+        'time': time,
     }
 
-    expected = {
+
+@pytest.fixture
+def assert_response(action_name, user, time, data, code):
+    return {
         'action': action_name,
         'user': user,
-        'time': None,
+        'time': time,
         'data': data,
         'code': code
     }
 
-    response = make_response(request, code, data)
+
+def test_make_response(valid_request, code, data, assert_response):
+    response = make_response(valid_request, code, data)
 
     for name, value in response.items():
         if name != 'time':
-            assert expected.get('name') == response.get('name')
+            assert assert_response.get('name') == response.get('name')
