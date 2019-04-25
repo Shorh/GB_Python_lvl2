b97a4e9122d528da6929f37bb1aa6f4cd20d4dc4
Shorh <Shorhina@gmail.com>
2019-04-25 17:15:53 +0500

    decorator for login

--- server/decorators.py
+++ server/decorators.py
@@ -2,6 +2,8 @@ import logging
 
 from functools import wraps
 
+from protocol import make_403
+
 
 logger = logging.getLogger('decorators')
 
@@ -11,5 +13,15 @@ def logged(func):
     def wrapper(request, *args, **kwargs):
         logger.debug(f'{ func.__name__ }: { request }')
         return func(request, *args, **kwargs)
+
+    return wrapper
+
+
+def login_required(func):
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        if request.get('user'):
+            return func(request, *args, **kwargs)
+        return make_403(request)
 
     return wrapper
