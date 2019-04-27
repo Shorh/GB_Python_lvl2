27e8ed3ed0de79615753c3ace866fb7853d9ce9b
Shorh <Shorhina@gmail.com>
2019-04-27 18:00:51 +0500

    test_validate_request with invalid action and invalid time

--- server/tests/protocol/test_validate_request.py
+++ server/tests/protocol/test_validate_request.py
@@ -23,11 +23,44 @@ def valid_request(action_name, time):
 
 
 @pytest.fixture
-def assert_response():
+def invalid_request_action(action_name, time):
+    return {
+        'action': None,
+        'time': time,
+    }
+
+
+@pytest.fixture
+def invalid_request_time(action_name, time):
+    return {
+        'action': action_name,
+        'time': None,
+    }
+
+
+@pytest.fixture
+def assert_response_true():
     return True
 
 
-def test_validate_request(valid_request, assert_response):
+@pytest.fixture
+def assert_response_false():
+    return False
+
+
+def test_validate_request_true(valid_request, assert_response_true):
     response = validate_request(valid_request)
 
-    assert assert_response == response
+    assert assert_response_true == response
+
+
+def test_validate_request_false_action(invalid_request_action, assert_response_false):
+    response = validate_request(invalid_request_action)
+
+    assert assert_response_false == response
+
+
+def test_validate_request_false_time(invalid_request_time, assert_response_false):
+    response = validate_request(invalid_request_time)
+
+    assert assert_response_false == response
